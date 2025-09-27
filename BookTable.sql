194b291b621ca25bd070693ab0eccc7a6fbf561b
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-09-28 01:08:41 +0530

    Add files via upload

---
+++ BookTable.sql
@@ -0,0 +1,9 @@
+// Create books table
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    year_published INT,
+    price DECIMAL(10, 2)
+);
