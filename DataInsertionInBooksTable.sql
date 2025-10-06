f66b8e7adfa2ef04af618d279267fde98923e799
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-07 01:26:10 +0530

    Add files via upload

---
+++ DataInsertionInBooksTable.sql
@@ -0,0 +1,9 @@
+//Data insertion in books table
+
+-- Insert data into the Books table
+INSERT INTO Books (book_id, title, author, year_published, price) VALUES
+(1, 'The Great Gatsby', 'F. Scott', 1925, 10.99),
+(2, '1984', 'George Orwell', 1949, 8.99),
+(3, 'To Kill a Mockingbird', 'Harper Lee', 1960, 12.50),
+(4, 'Moby Dick', 'Herman Melville', 1851, 15.00),
+(5, 'War and Peace', 'Leo Tolstoy', 1869, 20.00);
