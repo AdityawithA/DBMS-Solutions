c49fdd05b55ca9f4f7abfd1007ea6cc455f297af
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-09 06:33:46 +0530

    Add files via upload

---
+++ DataInsertionInNewCustomerTable.sql
@@ -0,0 +1,8 @@
+//Insert data into the Customers table
+
+INSERT INTO Customers (CustomerID, CustomerName, Country) VALUES
+(1, 'Alice', 'USA'),
+(2, 'Bob', 'Canada'),
+(3, 'Charlie', 'UK'),
+(4, 'David', 'USA'),
+(5, 'Eva', 'Germany');
