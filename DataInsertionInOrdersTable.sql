efc3138a13eecd427508178fc0281b3467fc21ad
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-10 06:48:45 +0530

    Add files via upload

---
+++ DataInsertionInOrdersTable.sql
@@ -0,0 +1,13 @@
+//Data Insertion In Orders Table.
+
+
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1001, 1, 101, 1);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1002, 2, 103, 2);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1003, 1, 105, 1);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1004, 3, 102, 1);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1005, 4, 104, 3);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1006, 5, 101, 2);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1007, 2, 105, 1);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1008, 3, 104, 1);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1009, 4, 103, 2);
+INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity) VALUES (1010, 5, 102, 1);
