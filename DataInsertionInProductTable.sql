daa82d6b2da94d073c2b060d80beb25ae379ff8e
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-11 14:28:58 +0530

    Add files via upload

---
+++ DataInsertionInProductTable.sql
@@ -0,0 +1,7 @@
+//Data insertion in product table
+
+INSERT INTO Products (ProductID, ProductName, Price) VALUES (101, 'Laptop', 1200.00);
+INSERT INTO Products (ProductID, ProductName, Price) VALUES (102, 'Smartphone', 800.00);
+INSERT INTO Products (ProductID, ProductName, Price) VALUES (103, 'Tablet', 450.00);
+INSERT INTO Products (ProductID, ProductName, Price) VALUES (104, 'Headphones', 150.00);
+INSERT INTO Products (ProductID, ProductName, Price) VALUES (105, 'Smartwatch', 300.00);
