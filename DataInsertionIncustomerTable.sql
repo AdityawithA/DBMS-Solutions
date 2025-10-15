c64aeaf3cb2356e7c5099fae2a65f2b22029282b
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-15 08:35:35 +0530

    Update DataInsertionIncustomerTable.sql

--- DataInsertionIncustomerTable.sql
+++ DataInsertionIncustomerTable.sql
@@ -6,4 +6,5 @@ INSERT INTO Customers (customer_id, name, city, join_date) VALUES
 (102, 'Bob Smith', 'Los Angeles', '2019-03-22'),
 (103, 'Carol White', 'Chicago', '2021-11-05'),
 (104, 'David Brown', 'New York', '2022-01-15'),
+
 (105, 'Eve Davis', 'San Francisco', '2020-07-30');
