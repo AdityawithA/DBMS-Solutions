e0c0c1cfa891f1f3107edbf528e92971b3772d17
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-16 13:47:44 +0530

    Add files via upload

---
+++ select_usa_customers.sql
@@ -0,0 +1,9 @@
+//Find the names of all customers from the USA.
+
+
+SELECT CustomerName FROM Customers WHERE Country = 'USA';
+
+| CustomerName |
+|--------------|
+| Alice        |
+| David        |
