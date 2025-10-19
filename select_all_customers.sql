7d29565ff74acc98307ac070bce12656a14234b1
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-19 22:13:33 +0530

    Add files via upload

---
+++ select_all_customers.sql
@@ -0,0 +1,12 @@
+//List all customers from the table.
+
+
+SELECT * FROM Customers;
+
+| CustomerID | CustomerName | Country |
+|------------|--------------|---------|
+| 1          | Alice        | USA     |
+| 2          | Bob          | Canada  |
+| 3          | Charlie      | UK      |
+| 4          | David        | USA     |
+| 5          | Eva          | Germany |
