94994bf325896bbfb8a3a2079b3ec5349424b4f6
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-10-21 00:20:58 +0530

    Add files via upload

---
+++ select_all_books_with_authors.sql
@@ -0,0 +1,12 @@
+//Select all books with their titles and authors.
+
+SELECT title, author
+FROM Books;
+
+| title                  | author           |
+|------------------------|------------------|
+| The Great Gatsby       | F. Scott         |
+| 1984                   | George Orwell    |
+| To Kill a Mockingbird  | Harper Lee       |
+| Moby Dick              | Herman Melville  |
+| War and Peace          | Leo Tolstoy      |
